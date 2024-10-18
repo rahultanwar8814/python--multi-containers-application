7385a62baadfbcd50c088ab3f184449de1567a2d
rahul <rahul.tanwar@valueinnovationlabs.com>
2024-10-18 14:49:10 +0530

    update code bass on kubernetes

--- front-end/frontend-main.py
+++ front-end/frontend-main.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    response = requests.get('http://localhost:5000/api/data')  # Adjust the service name as needed
+    response = requests.get('http://backend-service:5000/api/data')  # Use the backend service name
     data = response.json()
     return render_template('index.html', message=data['message'])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
