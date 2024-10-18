a024a79c7de7871eae5c0d9697904e7ae4e3a7b4
rahul <rahul.tanwar@valueinnovationlabs.com>
2024-10-18 16:02:49 +0530

    minor changes

--- back-end/backend-main.py
+++ back-end/backend-main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({"message": "Hello from the backend!"})
+    return jsonify({"message": "Hello from the backend and End!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
